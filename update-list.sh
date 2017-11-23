b4df9e19b2c659edd6cb5b18c98dc312ad4aa8dc
squatched <squatched@users.noreply.github.com>
2017-11-22 22:40:05 -0800

    Initial seeding with very simple blacklist and whitelist and script to update an ipset with a list (or lists).

---
+++ update-list.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+target_ipset="$1"
+shift
+
+echo Getting whitelist(s)...
+echo "" >/tmp/merged-list.txt
+
+while [[ ${#} -gt 0 ]]; do
+	curl -o "/tmp/${1}" "https://raw.githubusercontent.com/squatched/network-lists/master/${1}"
+	cat "/tmp/${1}" >>/tmp/merged-list.txt
+	rm "/tmp/${1}"
+	shift
+done
+
+echo Sorting the merged list and removing any dupes...
+sort -u /tmp/merged-list.txt >/tmp/sorted-list.txt
+
+get_list_script() {
+	cat "create temp-ipset nethash --hashsize 1024 --probes 4 --resize 20"
+	cat /tmp/sorted-list.txt | sed -e "s/^/add temp-ipset/g"
+}
+
+# Create the ip set
+get_list_script >/tmp/list-ipset.txt
+sudo ipset -exist restore </tmp/list-ipset.txt
+sudo ipset swap temp-ipset ${target_ipset}
+sudo ipset destroy temp-ipset
+
+# Cleanup cleanup, everybody, everywhere
+echo Cleanup...
+rm /tmp/merged-list.txt
+rm /tmp/sorted-list.txt
+rm /tmp/list-ipset.txt
